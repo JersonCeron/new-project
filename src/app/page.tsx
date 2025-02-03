d44395a7efe53701382be98c24d1d361f94d9dda
JersonCeron <[jjavierceron23@gmail.com]>
2025-02-02 22:24:41 -0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,248 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="page">
+      <div className="banner">
+        <div className="left1">
+          <div className="title1">
+            <div className="line-top">
+              <img className="line-top-img" src="./line-top.png" alt="" />
+            </div>
+            <h1>Lo mejor de crypto <br /> en un solo lugar.</h1>
+            <div className="line-bot">
+              <img className="line-bot-img" src="./line-bot.png" alt="" />
+            </div>
+          </div>
+          <div className="text1">
+            <p>
+              En CXP, accede a un equipo de expertos,<br /> 
+              herramientas especializadas, formación <br />
+              en criptomonedas y una comunidad <br /> 
+              activa, todo lo que necesitas en un solo <br /> 
+              lugar.
+            </p>
+          </div>
+          <div className="btn">Regístrate</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="right1">
+            <img className="img-banner" src=".\sabio.png" alt="" />
+        </div>
+      </div>
+      <div className="carrusel">
+        <div className="separator"></div>
+        <div className="icon"></div>
+        <div className="numers">
+          <div className="number1"></div>
+          <div className="number2"></div>
+        </div>
+        <div className="arrow"></div>
+      </div>
+      <div className="cicle">
+        <div className="left2">
+          <img className="img-cicle" src="./img-cicle.png" alt="" />
+        </div>
+        <div className="right2">
+          <div className="title2">
+            <div className="icon-title"><img src="./book.png" alt="" /></div>
+            <h2>Prepárate para el ciclo <br />
+            más grande de <span>la historia <br />
+            de las cryptos.</span> </h2>
+          </div>
+          <div className="text2">
+          Nuestro equipo ha transformado más de 10 años de experiencia <br />
+          en una rápida y sencilla curva de aprendizaje, estamos <br />
+          definitivamente en el mejor momento para el mercado Crypto <br />
+          y en CXP te traemos todo lo necesario para que tú también puedas <br />
+          ser parte de esto. 
+          </div>
+          <div className="btn-cicle">
+            Empieza Ahora
+          </div>
+        </div>
+      </div>
+      <div className="benefist">
+        <div className="article">
+          <div className="title3">
+            <div className="icon-title"><img src="./benefist.png" alt="" /></div>
+            <h2>Aprender a <span>operar cryptos </span><br /> 
+            nunca había sido tan fácil </h2>
+            <div className="separator">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+          </div>
+          <div className="article-content">
+            <div className="article-card">
+              <div>
+                <img className="img" src="./benefist-img1.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+            <div className="article-card">
+              <div>
+                <img className="img" src="./benefist-img2.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+            <div className="article-card">
+              <div>
+                <img className="img" src="./benefist-img3.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+            <div className="article-card">
+              <div>
+                <img className="img" src="./benefist-img4.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="transform">
+        <div className="img-transform">
+          <img className="img-int" src="./transform.png" alt="" />
+        </div>
+        <div className="title4">
+          <div className="line-top">
+              <img className="line-top-img2" src="./line-top.png" alt="" />
+          </div>
+          <h2>
+          Transforma tus finanzas <br />
+          con la <span>economía del futuro</span>
+          </h2>
+          <div className="line-bot">
+              <img className="line-bot-img2" src="./line-bot.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="experts">
+        <div className="title3">
+            <div className="icon-title"><img src="./experts-icon.png" alt="" /></div>
+            <h2> <span>Aprende y opera</span> con <br />
+            nusestros expertos</h2>
+            <div className="separator">
+              <div className="circle"></div>
+              <div className="line"></div>
+            </div>
+        </div>
+        <div className="article-content">
+            <div className="article-card">
+              <div>
+                <img className="img" src="./expert-img1.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+            <div className="article-card">
+              <div>
+              <img className="img" src="./expert-img2.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+            <div className="article-card">
+              <div>
+              <img className="img" src="./expert-img3.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+            <div className="article-card">
+              <div>
+              <img className="img" src="./expert-img4.png" alt="" />
+              </div> 
+              <div className="content-card">
+                <div className="title-card">Lorem ipsum dolor sit amet.</div>
+                <div className="text-card">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+                </div>
+                <div className="btn-card">Ver más</div>
+              </div>
+            </div>
+          </div>
+      </div>
+      <div className="users">
+        <div className="left3">
+        <div><img className="img-users" src="./users-img.png" alt="" /></div>
+        </div>
+        <div className="right3">
+        <div className="users-content">
+          <div className="users-title"><h2>Lo que dicen nuestros Usuarios</h2></div>
+          <div className="users-card">
+            <div className="users-text">“Las señales de CXP me han ayudado a mejorar mi estrategia de trading. No son infalibles, pero definitivamente son una herramienta valiosa en mi arsenal”</div>
+            <div className="users-autor"><span>Ana R.</span> Trader de Criptomonedas.</div>
+          </div>
+        </div>
+        <div className="users-card">
+            <div className="users-text">“Las señales de CXP me han ayudado a mejorar mi estrategia de trading. No son infalibles, pero definitivamente son una herramienta valiosa en mi arsenal”</div>
+            <div className="users-autor"><span>Carlos M. </span> Inversor en Criptomonedas.</div>
+          </div>
+        </div>
+      </div>
+      <div className="finanzas">
+        <div className="left2">
+          <img className="finanzas-img" src="./finanzas-img.png" alt="" />
+        </div>
+        <div className="right2">
+          <div className="title2">
+            <h2>
+            Somos el mejor aliado para <br /> 
+            <span>potencializar tus finanzas</span>
+            </h2>
+          </div>
+          <div className="text2">
+            En CXP estamos motivados en potencializar la adopción masiva <br /> 
+            de las criptomonedas facilitándote todas nuestras herramientas <br />
+            y conocimientos a nuestra comunidad, nuestro equipo de expertos <br />
+            con más de 10 años de experiencia ha desarrollado herramientas <br />
+            para llevar tus finanzas al siguiente nivel.
+          </div>
+          <div className="social-icons">
+            <img className="social" src="./telegram-icon.png" alt="" />
+            <img className="social" src="./discord-icon.png" alt="" />
+            <img className="social" src="./kick-icon.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
